25d65f2ff3efe8ecc4ae1a142cc3a41589965728
Balamurugan <balarvs2002@gmail.com>
2016-07-14 15:05:35 +0530

    Add files via upload

---
+++ Driver.cs
@@ -0,0 +1,52 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BehaviouralTest.WebUI.Framework
+{
+    public class Driver
+    {
+        
+        public static IWebDriver WebDriverInstance { get; private set; }
+
+        public static void Initialize()
+        {
+            if (WebDriverInstance == default(IWebDriver))
+            {
+                WebDriverInstance = new ChromeDriver();
+                //WebDriverInstance.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
+                WebDriverInstance.Manage().Window.Maximize();
+            }
+            
+        }
+
+        public static void Close()
+        {
+            if (WebDriverInstance == default(IWebDriver)) return;
+
+            WebDriverInstance.Close();
+        }
+
+        public static void Quit()
+        {
+            if (WebDriverInstance == default(IWebDriver)) return;
+
+            WebDriverInstance.Quit();
+        }
+
+        //public static void Navigate(string URL)
+        //{
+        //    if (WebDriverInstance == default(IWebDriver))
+        //    {
+        //        throw new NullReferenceException("WebDriver instance is null");
+        //    }
+
+        //    WebDriverInstance.Navigate().GoToUrl(URL);
+
+        //}
+    }
+}
---
+++ DriverNavigation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BehaviouralTest.WebUI.Framework
+{
+    public static class DriverNavigation
+    {
+        
+        static DriverNavigation()
+        {
+            Driver.Initialize();
+        }
+
+        public static void Navigate(string URL)
+        {
+            if (Driver.WebDriverInstance == default(object))
+            {
+                throw new NullReferenceException("WebDriver instance is null");
+            }
+
+            Driver.WebDriverInstance.Navigate().GoToUrl(URL);
+        }
+
+    }
+}
